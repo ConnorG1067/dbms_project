1fe474bc5db21a39b2041d5b2bba400346b985ae
ConnorG1067 <77849049+ConnorG1067@users.noreply.github.com>
2023-12-02 17:41:09 -0500

    Updated dashboard ui

--- lib/features/home/main_dashboard.dart
+++ lib/features/home/main_dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:dbms_project/Widgets/dashboard_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class MainDashboard extends StatefulWidget {
   const MainDashboard({Key? key}) : super(key: key);
@@ -11,21 +12,44 @@ class MainDashboard extends StatefulWidget {
 class _MainDashboardState extends State<MainDashboard> {
 
   List<Widget> cardList = [
-    DashboardWidgets.dashboardCard(160, 250, 'Weight'),
-    DashboardWidgets.dashboardCard(1500, 3200, 'Calories'),
-    DashboardWidgets.dashboardCard(3700, 10000, 'Steps'),
-    DashboardWidgets.dashboardCard(500, 3700, 'Hydration')
+
 
   ];
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-        physics: const AlwaysScrollableScrollPhysics(),
-        crossAxisCount: 3,
-        crossAxisSpacing: 4.0,
-        mainAxisSpacing: 8.0,
-        children: cardList
+    return ListView(
+      children: [
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  SizedBox(width: 340, height: 340, child: DashboardWidgets.dashboardCard(160, 250, 'Weight')),
+                  SizedBox(width: 340, height: 340, child: DashboardWidgets.dashboardCard(1500, 3200, 'Calories')),
+                  SizedBox(width: 340, height: 340, child: DashboardWidgets.dashboardCard(3700, 10000, 'Steps')),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  SizedBox(width: 340, height: 340, child: DashboardWidgets.dashboardCard(500, 3700, 'Hydration')),
+                  SizedBox(width: 340, height: 340, child: DashboardWidgets.dashboardCard(30, 180, 'Active Minutes')),
+                  SizedBox(width: 340, height: 340, child: DashboardWidgets.dashboardCard(1, 8, 'Sleep Duration')),
+                ],
+              )
+            ],
+          ),
+        ),
+        TableCalendar(
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: DateTime.now(),
+        )
+      ],
     );
   }
 }
