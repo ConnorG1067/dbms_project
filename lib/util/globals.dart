d23ee5bb5be181b645020f462781302d0f78e640
ConnorG1067 <77849049+ConnorG1067@users.noreply.github.com>
2023-12-09 13:48:31 -0500

    Added scheduling system, add and delete

--- lib/util/globals.dart
+++ lib/util/globals.dart
@@ -3,4 +3,5 @@ import 'package:postgres/postgres.dart';
 class Globals{
   static late PostgreSQLConnection database;
   static late Map<String, dynamic> currentAccount;
+  static late List<PostgreSQLResultRow> trainers;
 }
